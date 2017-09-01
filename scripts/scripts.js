3f8d2268d4ba3803b42b355d0e435d6501bd35ac
Michael Du Toit <michael.dtoit@gmail.com>
2017-09-01 13:00:03 +1200

    Site Updated: 01-09-2017 12:59:59

---
+++ scripts/scripts.js
@@ -0,0 +1,77 @@
+var mobileBreak = 750;
+var navMenuStatus = true;
+var menuBtn = document.getElementById('menuBtn');
+var menuLines = document.getElementById('menuLines');
+var bodyEl = document.getElementsByTagName('body')[0];
+var htmlEl = document.getElementsByTagName("html")[0];
+
+
+function menu(){
+    var menu_el = document.getElementById('menu');
+    if (navMenuStatus == false){
+        menu_el.classList.add('hide');
+        menu_el.classList.remove('show');
+        menuLines.classList.remove('change');
+        navMenuStatus = true;
+    } else {
+        menu_el.classList.add('show');
+        menu_el.classList.remove('hide');
+        menuLines.classList.add('change');
+        navMenuStatus = false;
+    }
+};
+menuBtn.addEventListener('click', menu);
+
+function isMobile(){
+    if(bodyEl.clientWidth <= mobileBreak || document.documentElement.clientWidth <= mobileBreak){
+        menuBtn.classList.add('show');
+        menuBtn.classList.remove('hide');
+        navMenuStatus = false;
+        menu();
+    }
+    else {
+        menuBtn.classList.add('hide');
+        menuBtn.classList.remove('show');
+        navMenuStatus = true;
+        menu();
+    }
+};
+window.addEventListener('load', isMobile);
+window.addEventListener('resize', isMobile);
+
+function scrollTo(element, to, duration){
+    if(duration <= 0){
+        return false;
+    }
+    var diff = to - element.scrollTop;
+    var speed = diff / duration * 10;
+    setTimeout(function(){
+        element.scrollTop = element.scrollTop + speed;
+        if (element.scrollTop === to){
+            return;
+        }
+        scrollTo(element,to,duration - 10);
+    }, 5)
+};
+var toTopBtn = document.getElementById('to-top');
+function scrollToTop(){
+    if(document.body.scrollTop != 0 || document.documentElement.scrollTop != 0){
+        scrollTo(bodyEl,0,400);
+        scrollTo(htmlEl,0,400);
+    }
+    else {
+        return false;
+    }
+};
+toTopBtn.addEventListener('click', scrollToTop);
+
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('/service-worker.js', {scope: '/'}).then(function(registration){
+            console.log('ServiceWorker registration successfull with scope: ', registration.scope);
+        }).catch(function(err){
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+};
---
+++ service-worker.js
@@ -0,0 +1,81 @@
+const version = 'MDT-v2-';
+const coreCacheName = version + 'core';
+const pagesCacheName = version + 'pages';
+const assetsCacheName = version + 'assets';
+
+const coreCacheUrls = [
+    '/js/scripts.js',
+    '/css/main.css',
+    '/images/woodland.jpg',
+    '/offline/',
+];
+
+function updateCoreCache(){
+    return caches.open(coreCacheName).then(
+        cache => {
+            return cache.addAll(coreCacheUrls);
+        }
+    );
+};
+
+function addToCache(cacheName, request, response){
+    caches.open(cacheName).then(cache => cache.put(request, response));
+};
+
+function clearCaches(){
+    return caches.keys().then(function(keys){
+        return Promise.all(keys.filter(function(key){
+            return key.indexOf(version) !== 0;
+        }).map(function(key){
+            return caches.delete(key);
+        })
+    );
+    })
+}
+self.addEventListener('install', event => {
+    event.waitUntil(updateCoreCache().then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        clearCaches().then(() => {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    let request = event.request;
+    let acceptHeader = request.headers.get('Accept');
+    if(acceptHeader.indexOf('text/html') !== -1){
+        event.respondWith(
+            fetch(request).then(response => {
+                addToCache(pagesCacheName, request, response.clone());
+                return response;
+            })
+            .catch(()=>{
+                return caches.match(request).then(response => {
+                    return response || caches.match('/offline/');
+                })
+            })
+        );
+    }
+    else if (acceptHeader.indexOf('text/html') == -1){
+        event.respondWith(
+            caches.match(request)
+            .then(response => {
+                return response || fetch(request)
+                    .then( response => {
+                        addToCache(assetsCacheName, request, response.clone());
+                        return response;
+                    })
+                    .catch(() => {
+                        return new Response('<div style="background:grey;color:white;font-weight:bold;display:block">Offline</div>');
+                    })
+            })
+        );
+    }
+});
+
+
