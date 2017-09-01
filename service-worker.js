07b9f3f3f087dea644e204b7c37518860c0d551b
Michael Du Toit <michael.dtoit@gmail.com>
2017-09-01 13:39:33 +1200

    Site Updated: 01-09-2017 13:39:32

--- service-worker.js
+++ service-worker.js
@@ -65,7 +65,7 @@ self.addEventListener('fetch', event => {
         event.respondWith(
             caches.match(request)
             .then(response => {
-                return fetch(request) || response
+                return response || fetch(request)
                     .then( response => {
                         addToCache(assetsCacheName, request, response.clone());
                         return response;
